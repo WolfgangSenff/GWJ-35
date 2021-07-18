1b6c5a6706aa553f83eeee239136eb22defe70e1
D4yz <55066555+D4yz-Ag0@users.noreply.github.com>
2021-07-18 15:37:34 -0400

    Title Added

--- scenes/EndDoor.gd
+++ scenes/EndDoor.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 func _ready():
-    yield(get_tree(), "idle_frame")
-    z_index = get_parent().get_node("FG").z_index
+	yield(get_tree(), "idle_frame")
+	z_index = get_parent().get_node("FG").z_index
 
 func _on_EndDoor_body_entered(body: Node) -> void:
-    if body.z_index == z_index - 2:
-        get_tree().change_scene("res://scenes/Credits.tscn")
+	if body.z_index == z_index - 2:
+		get_tree().change_scene("res://scenes/Credits.tscn")
