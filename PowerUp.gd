e7c179a0f8bbd5fe87348e2b4c23a13ab58ebc4b
D4yz <55066555+D4yz-Ag0@users.noreply.github.com>
2021-07-18 12:25:53 -0400

    Check in.

--- PowerUp.gd
+++ PowerUp.gd
@@ -6,7 +6,7 @@ signal gauntlets_unlocked
 onready var animation_player = $AnimationPlayer
 
 func _on_PowerUp_body_entered(body: Node) -> void:
-    animation_player.play("Got")
-    yield(animation_player, "animation_finished")
-    emit_signal("gauntlets_unlocked")
-    $CollisionShape2D.disabled = true
+	animation_player.play("Got")
+	yield(animation_player, "animation_finished")
+	emit_signal("gauntlets_unlocked")
+	$CollisionShape2D.disabled = true
