8fecf9e4a0dc9de0324d715a06b0abff6fa41d21
D4yz <55066555+D4yz-Ag0@users.noreply.github.com>
2021-07-18 14:40:48 -0400

    Level Complete

--- scenes/EndDoor.gd
+++ scenes/EndDoor.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 func _ready():
-    z_index = get_parent().get_node("FG").z_index
+	z_index = get_parent().get_node("FG").z_index
 
 func _on_EndDoor_body_entered(body: Node) -> void:
-    if body.z_index == z_index:
-        get_tree().change_scene("res://scenes/Credits.tscn")
+	if body.z_index == z_index:
+		get_tree().change_scene("res://scenes/Credits.tscn")
--- scenes/Transfer.gd
+++ scenes/Transfer.gd
@@ -1,5 +1,5 @@
 extends Area2D
 
 func _on_Transfer_body_entered(body: Node) -> void:
-    body.position.y = wrapf(body.position.y, 0, get_viewport_rect().size.y)
-    get_tree().call_group("Game", "transfer_backward")
+	body.position.y = wrapf(body.position.y, 0, get_viewport_rect().size.y)
+	get_tree().call_group("Game", "transfer_backward")
